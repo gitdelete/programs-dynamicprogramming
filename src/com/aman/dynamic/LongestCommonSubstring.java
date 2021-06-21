4a03c804ac4d29b0e2832fb6369239dec3b01c71
gitdelete <amanjotsingh10@gmail.com>
2021-06-21 17:42:10 +0530

    longest common substring between 2 strings

---
+++ src/com/aman/dynamic/LongestCommonSubstring.java
@@ -0,0 +1,74 @@
+package com.aman.dynamic;
+
+public class LongestCommonSubstring {
+
+    static int LCSubStr(char X[], char Y[],
+                        int m, int n)
+    {
+        // Create a table to store
+        // lengths of longest common
+        // suffixes of substrings.
+        // Note that LCSuff[i][j]
+        // contains length of longest
+        // common suffix of
+        // X[0..i-1] and Y[0..j-1].
+        // The first row and first
+        // column entries have no
+        // logical meaning, they are
+        // used only for simplicity of program
+        int LCStuff[][] = new int[m + 1][n + 1];
+
+        // To store length of the longest
+        // common substring
+        int result = 0;
+
+        // Following steps build
+        // LCSuff[m+1][n+1] in bottom up fashion
+        for (int i = 0; i <= m; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                if (i == 0 || j == 0)                       //String X = "OldSite:GeeksforGeeks.org";
+                                                            // String Y = "NewSite:GeeksQuiz.com";
+                    LCStuff[i][j] = 0;
+                else if (X[i - 1] == Y[j - 1])
+                {
+                    LCStuff[i][j]
+                            = LCStuff[i - 1][j - 1] + 1;
+                    result = Integer.max(result,
+                            LCStuff[i][j]);
+                }
+                else
+                    LCStuff[i][j] = 0;
+            }
+        }
+
+        printSolution(LCStuff,m,n);
+
+        return result;
+    }
+
+    static void printSolution(int queens[][], int m, int n)
+    {
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++)
+                System.out.print(" " + queens[i][j]
+                        + " ");
+            System.out.println();
+        }
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+        String X = "OldSite:GeeksforGeeks.org";
+        String Y = "NewSite:GeeksQuiz.com";
+
+        int m = X.length();
+        int n = Y.length();
+
+        System.out.println(LCSubStr(X.toCharArray(),
+                Y.toCharArray(), m,
+                n));
+    }
+}
--- src/com/aman/dynamic/NumberOfPalindrome.java
+++ src/com/aman/dynamic/NumberOfPalindrome.java
@@ -5,6 +5,8 @@ public class NumberOfPalindrome {
     public static void main(String[] args) {
 
 
+
+
     }
 
 }
