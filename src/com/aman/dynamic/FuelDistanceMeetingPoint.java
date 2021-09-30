21f131bbeb6f509145c1126e64e963fe0baba15f
Amanjot Singh <amanjotsingh10@gmail.com>
2021-09-30 20:19:52 +0530

    fuel distance visit all petrol pump

---
+++ src/com/aman/dynamic/FuelDistanceMeetingPoint.java
@@ -0,0 +1,37 @@
+package com.aman.dynamic;
+
+public class FuelDistanceMeetingPoint {
+
+    public static void main(String[] args) {
+        int[] fuelArray =     {0,5,6,10,2,3};
+        int[] distanceArray = {4,3,8,7,5,3};
+        int size = fuelArray.length;
+        int fuelExtra= 0;
+        int start=0;
+        int end=1;
+        fuelExtra=fuelExtra+fuelArray[start]-distanceArray[start];
+        boolean endAgain=false;
+        while(end  != start){
+            if(fuelExtra<0){
+                fuelExtra = 0;
+                start=end;
+            }
+            fuelExtra = fuelExtra+fuelArray[end]-distanceArray[end];
+            end++;
+            if(start < end && endAgain) {
+                System.out.println("No Solution");
+                return;
+            }
+
+            if(end>size-1){
+                endAgain=true;
+            }
+            end=end%size;
+        }
+
+        if(start == end){
+            System.out.println(start + " "+ end);
+        }
+
+    }
+}
--- src/com/aman/dynamic/GroupAnagramsTogetherInArray.java
+++ src/com/aman/dynamic/GroupAnagramsTogetherInArray.java
@@ -1,9 +1,54 @@
 package com.aman.dynamic;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class GroupAnagramsTogetherInArray {
 
     public static void main(String[] args) {
 
+        String wordArr[] = { "cat", "dog", "tac", "god", "act", "cow","owc", "owl" };
+        int size = wordArr.length;
+        printAnagramsTogether(wordArr, size);
+    }
+
+    private static void printAnagramsTogether(String[] wordArr, int size) {
+
+        Map<String, List<String>> wordsMap = new HashMap<>();
+        int[] charArray1 = new int[26];
+        Arrays.fill(charArray1,0);
+        int[] charArray2 = new int[26];
+        Arrays.fill(charArray2,0);
+        /* System.out.println(Integer.valueOf('a'));
+        System.out.println(Integer.valueOf('z'));
+        System.out.println('a'-97);
+        System.out.println('z'-97); */
+        boolean[] wordDone = new boolean[wordArr.length] ;
+        Arrays.fill(wordDone,false);
+        for(int i=0; i<size; i++){
+
+            if(!wordDone[i]){
+                for (int c = 0; c < wordArr[i].length(); c++) {
+                    charArray1[wordArr[i].charAt(c) - 97]++;
+                }
+                List<String> list = new ArrayList<>();
+                list.add(wordArr[i]);
+                wordsMap.put(wordArr[i], list);
+
+                for (int j = i + 1; j < size; j++) {
+                    for (int c = 0; c < wordArr[j].length(); c++) {
+                        charArray2[wordArr[j].charAt(c) - 97]++;
+                    }
+                    if (Arrays.equals(charArray1, charArray2)) {
+                        wordsMap.get(wordArr[i]).add(wordArr[j]);
+                        wordDone[j] = true;
+                    }
+                    Arrays.fill(charArray2, 0);
+                }
+            }
+            Arrays.fill(charArray1,0);
+        }
+        wordsMap.forEach( (K,V)-> V.forEach(a->System.out.print(a + " ")));
     }
 
 }
