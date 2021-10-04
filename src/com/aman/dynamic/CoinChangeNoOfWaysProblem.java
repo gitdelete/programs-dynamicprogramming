17fef6d7456bc2119c6d440244e93d65746832e6
Amanjot Singh <amanjotsingh10@gmail.com>
2021-10-04 18:38:11 +0530

    DP

--- src/com/aman/dynamic/CoinChangeNoOfWaysProblem.java
+++ src/com/aman/dynamic/CoinChangeNoOfWaysProblem.java
@@ -1,9 +1,82 @@
 package com.aman.dynamic;
 
-//Unbouded 0-1 knapsack
+//Unbounded 0-1 knapsack
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/*
+ *              0   1   2   3   4   5
+ *   coin   0   1   0   0   0   0   0
+ *    1     1   1   1   1   1   1   1
+ *    2     2   1   1   2   2   3   3
+ *    3     3   1   1   2   3   4   5
+ *    5     4   1   1   2   3   4   6
+ *
+ *  backtrack to find the weights selected
+ *
+ *
+ */
 public class CoinChangeNoOfWaysProblem {
+
+
 
     public static void main(String[] args) {
 
+        int[] coins = {1,5,10};
+        int result = 12;
+        System.out.println(coinChange(coins, result));
+        System.out.println(coinChangeSpaceOptimize(coins, result));
+
+    }
+
+    private static int coinChange(int[] coins, int result) {
+
+        int[][] ways = new int[coins.length+1][result+1];
+        for(int i=0; i<coins.length+1; i++) {
+            for (int j = 0; j < result+1; j++) {
+
+                if (i == 0 && j > 0) {
+                    ways[i][j] = 0;
+                }
+                if (i >= 0 && j == 0) {
+                    ways[i][j] = 1;
+                }
+            }
+        }
+        for(int i=1; i<coins.length+1; i++){
+            for (int j=0; j<result+1; j++){
+
+                if(coins[i-1]>j){
+                    ways[i][j]=ways[i-1][j];
+                }
+                if(coins[i-1]<=j){
+                    ways[i][j]=ways[i-1][j]+ways[i][j-coins[i-1]];
+                }
+            }
+        }
+        for(int i=0; i<ways.length; i++) {
+            for (int j = 0; j < result; j++) {
+                System.out.print(ways[i][j]+ "   ");
+            }
+            System.out.println(" ");
+        }
+        return ways[coins.length][result];
+    }
+
+
+    private static int coinChangeSpaceOptimize(int[] coins, int result) {
+
+        int[] ways = new int[result+1];
+        ways[0]=1;
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = 0; j < ways.length; j++) {
+                if (coins[i] <= j) {
+                    ways[j] += ways[j - coins[i]];
+                }
+            }
+        }
+        return ways[result];
     }
 }
--- src/com/aman/dynamic/LCS/LongestCommonSubstring.java
+++ src/com/aman/dynamic/LCS/LongestCommonSubstring.java
@@ -5,25 +5,11 @@ public class LongestCommonSubstring {
     static int LCSubStr(char X[], char Y[],
                         int m, int n)
     {
-        // Create a table to store
-        // lengths of longest common
-        // suffixes of substrings.
-        // Note that LCSuff[i][j]
-        // contains length of longest
-        // common suffix of
-        // X[0..i-1] and Y[0..j-1].
-        // The first row and first
-        // column entries have no
-        // logical meaning, they are
-        // used only for simplicity of program
+
         int LCStuff[][] = new int[m + 1][n + 1];
 
-        // To store length of the longest
-        // common substring
         int result = 0;
 
-        // Following steps build
-        // LCSuff[m+1][n+1] in bottom up fashion
         for (int i = 0; i <= m; i++)
         {
             for (int j = 0; j <= n; j++)
