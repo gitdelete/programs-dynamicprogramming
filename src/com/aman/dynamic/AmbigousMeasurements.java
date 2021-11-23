1f865c6839960c3354713fb232a74083ea80fd44
Amanjot Singh <amanjotsingh10@gmail.com>
2021-11-23 15:08:36 +0530

    AmbiguousMeasurements

--- src/com/aman/dynamic/AmbigousMeasurements.java
+++ src/com/aman/dynamic/AmbigousMeasurements.java
@@ -4,16 +4,28 @@ public class AmbigousMeasurements {
 
     public static void main(String[] args) {
 
-        int[][] measuringCups = {
+        int[][] measuringCups1 = {
                 {200,210},
                 {450,465},
                 {800,850}
         };
-        int low=2100;
-        int high=2190;
+
+        int low1=2100;
+        int high1=2190;
         int lowSum = 0,highSum=0, row=0;
-        boolean b1 = measurement(measuringCups,low,high, lowSum, highSum, row);
+        boolean b1 = measurement(measuringCups1,low1,high1, lowSum, highSum, row);
         System.out.println(b1);
+        int[][] measuringCups2 = {
+                {1,3},
+                {2,4},
+                {5,6}
+        };
+
+        int low2=100;
+        int high2=120;
+        boolean b2 = measurement(measuringCups2,low2,high2, lowSum, highSum, row);
+
+        System.out.println(b2);
     }
 
     private static boolean measurement(int[][] measuringCups, int low, int high, int lowSum, int highSum,int row) {
@@ -30,10 +42,12 @@ public class AmbigousMeasurements {
             return true;
         }
 
-        if(measurement(measuringCups,low,high,lowSum+measuringCups[row][0],highSum+measuringCups[row][1],row))
+        if(measurement(measuringCups,low,high,lowSum+measuringCups[row][0],highSum+measuringCups[row][1],row)) {
             return true;
-        if(measurement(measuringCups,low,high,lowSum+measuringCups[row][0],highSum+measuringCups[row][1],row+1))
+        }
+        if(measurement(measuringCups,low,high,lowSum,highSum,row+1)) {
             return true;
+        }
 
 
         return false;
